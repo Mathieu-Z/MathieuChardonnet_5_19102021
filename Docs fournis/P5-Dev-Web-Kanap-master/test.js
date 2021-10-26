9c1aacdb0cb26fb83a5ffe309fe167dc0f874c4b
Mathieu Chardonnet <chardonnet.mathieu@gmail.com>
2021-10-26 15:55:46 +0200

    Update test.js
    tente des trucs

--- Docs fournis/P5-Dev-Web-Kanap-master/test.js
+++ Docs fournis/P5-Dev-Web-Kanap-master/test.js
@@ -1,6 +1,24 @@
+// récupération des données
 updateDisplay(
-    fetch().then(function(response) {
+    fetch('Product.js').then(function(response) {
     response.text().then(function(text) {
       poemDisplay.textContent = text;
     });
   }));
+
+// Boucle pour envoyer plusieurs données
+function repeat(){
+  if(i<n)n--
+  else(i<0)
+}
+
+// envoie des données en html
+document.addElementById("cart__items").innerHTML();
+
+// calcul prix article
+var articlePrice = (productPrice * numberOfProduct);
+
+// calcul prix panier
+var panierPrice = (articlePrice + articlePrice1)
+
+//
